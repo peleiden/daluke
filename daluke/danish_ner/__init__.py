3ff902d8b220380fcdc0dfaa1ea4b4aa54164a07
Søren Winkel Holm <swholm@protonmail.com>
2021-02-03 22:32:57 +0100

    Use pelutils parser for NER run

--- daluke/danish_ner/__init__.py
+++ daluke/danish_ner/__init__.py
@@ -20,6 +20,7 @@ class NER_TestModel(ABC):
     def setup(self):
         """ Set up the model, possibly downloading model files """
         raise NotImplementedError
+
     @abstractmethod
     def predict(self, words: str) -> list[str]:
         """ Predict the named entities of the input string `words` """
@@ -28,6 +29,7 @@ class NER_TestModel(ABC):
 class Bert(NER_TestModel):
     def setup(self):
         self.model = dm.load_bert_ner_model()
+
     def predict(self, words: str) -> list[str]:
         tokens, labels = self.model.predict(words)
         raise NotImplementedError #FIXME
@@ -36,6 +38,7 @@ class Flair(NER_TestModel):
     #FIXME: Dies on my machine due to CUDA only
     def setup(self):
         self.model = dm.load_flair_ner_model()
+
     def predict(self, words: str) -> list[str]:
         sentence = Sentence(words)
         self.model.predict(sentence)
@@ -45,6 +48,7 @@ class Flair(NER_TestModel):
 class Spacy(NER_TestModel):
     def setup(self):
         self.model = dm.load_spacy_model()
+
     def predict(self, words: str) -> list[str]:
         tokens = self.model(words)
         labels = [token.ent_type_ for token in tokens]
@@ -54,6 +58,7 @@ class Polyglot(NER_TestModel):
     # See: https://polyglot.readthedocs.io/en/latest/NamedEntityRecognition.html
     def setup(self):
         self.model = polyglot_text
+
     def predict(self, words: str) -> list[str]:
         text = self.model(words)
         labels = text.entities
@@ -65,6 +70,7 @@ class Daner(NER_TestModel):
         # FIXME: How to get programmatical access to this? daner is in java.
         # The best solution is probably to clone the repo and us os.system calls to run daner
         raise NotImplementedError
+
     def predict(self, words: str) -> list[str]:
         raise NotImplementedError
 
--- daluke/danish_ner/main.py
+++ daluke/danish_ner/main.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python3
-from pelutils import log
+from __future__ import annotations
+import os
 
 from daluke.danish_ner import setup_models
 
+from pelutils import log, Parser
+
+
+
 def main():
-    #TODO: Use actual argparsing here
+    options = dict()
+    parser = Parser(options, name="Danish NER Reproduction")
+    parser.parse()
+
     log.configure(
-        "local_test/danish_ner.log", "Benchmark danish NER models",
+        os.path.join(parser.location, "danish_ner.log"), "Benchmark danish NER models",
     )
+
     models = setup_models()
     log(f"Succesfully set up {len(models)} models")
+
 
 if __name__ == '__main__':
     with log.log_errors:
