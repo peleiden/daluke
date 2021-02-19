9b8488f901f3a23201ccf33135dcd7814975433f
Soren Winkel Holm <swholm@protonmail.com>
2021-02-19 10:43:38 +0100

    Run multiple repetitions

--- dev/luke.sh
+++ dev/luke.sh
@@ -1,22 +1,42 @@
 #!/bin/sh
 #BSUB -q gpuv100
 #BSUB -R "select[gpu32gb]"
-#BSUB -gpu "num=1:mode=exclusive_process"
-#BSUB -J "LUKE reproduction"
+#BSUB -gpu "num=1"
+#BSUB -J "LUKE-bench"
 #BSUB -R "rusage[mem=20GB]"
 #BSUB -n 1
 #BSUB -W 24:00
-#BSUB -o logs/%J.out
-#BSUB -e logs/%J.err
+#BSUB -o $HOME/joblogs/%J.out
+#BSUB -e $HOME/joblogs/%J.err
 #BSUB -u s183911@student.dtu.dk
 #BSUB -N
 
 echo "Running job"
-mkdir -p ../lukout
-python -m examples.cli \
-    --model-file=luke_large_500k.tar.gz \
-    --output-dir=../lukout \
-    ner run \
-    --data-dir=<DATA_DIR> \
-    --checkpoint-file=<CHECKPOINT_FILE> \
-    --no-train
+
+#python -m examples.cli \
+#    --model-file=data/luke_large_500k.tar.gz \
+#    --output-dir=data/lukout \
+#    ner run \
+#    --data-dir=data/CoNLL2003 \
+#    --checkpoint-file=data/pytorch_model.bin \
+#    --no-train
+
+SIZE=base
+for i in {1..5}
+do
+    OUTDIR="data/lukout$SIZE$i"
+    echo $OUTDIR
+    mkdir -p $OUTDIR
+
+    LR=$(["$SIZE" == "large"] && echo "1e-5" || echo "5e-5")
+    python -m examples.cli \
+        --model-file=data/luke_${SIZE}_500k.tar.gz \
+        --output-dir=$OUTDIR \
+        ner run \
+        --data-dir=data/CoNLL2003 \
+        --train-batch-size=8 \
+        --gradient-accumulation-steps=2 \
+        --learning-rate=$LR \
+        --num-train-epochs=5 \
+        --fp16
+done
