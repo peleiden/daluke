40261a0ff8ddab8af54d281fe85928ec80e5e11a
Søren Winkel Holm <swholm@protonmail.com>
2021-02-26 17:23:46 +0100

    Add capability for data module to show statistics

--- reproduction/danish_ner/data.py
+++ reproduction/danish_ner/data.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
+import os
+import sys
 from typing import Generator
 from abc import ABC, abstractmethod
-import os
 
 from danlp.datasets import DDT, WikiAnn
 
@@ -21,17 +22,17 @@ class TestDataset(ABC):
         """ List of words and list of entities (IOUB coded) """
 
 class Dane(TestDataset):
-    def setup(self):
+    def setup(self, split="test"):
         # Third element is test data
-        self.data = DDT().load_as_simple_ner(predefined_splits=True)[2]
+        self.data = DDT().load_as_simple_ner(predefined_splits=True)[("train", "dev", "test").index(split)]
 
     def get_data(self) -> tuple(list[str], list[str]):
         return self.data # data is already (text, truth)
 
 class Plank(TestDataset):
-    def setup(self, data_path: str):
+    def setup(self, data_path: str, split="test"):
         self.data: list[list[tuple[str]]] = list()
-        with open(os.path.join(data_path, "da_ddt-ud-ner-test.conll"), "r") as f:
+        with open(os.path.join(data_path, f"da_ddt-ud-ner-{split}.conll"), "r") as f:
             sentence = []
             for l in f:
                 if not l.split():
@@ -49,9 +50,9 @@ class Plank(TestDataset):
         return words, truth
 
 class Wikiann(TestDataset):
-    def setup(self, data_path: str):
+    def setup(self, data_path: str, split="test"):
         self.data: list[list[tuple[str]]] = list()
-        with open(os.path.join(data_path, "test"), "r") as f:
+        with open(os.path.join(data_path, split), "r") as f:
             sentence = []
             for l in f:
                 if not l.split():
@@ -74,7 +75,7 @@ ALL_DATASETS = (
     Wikiann("WikiANN"),
 )
 
-def setup_datasets(names_to_setup: list[str], wikiann_path: str="wikiann", plank_path: str="plank") -> list[TestDataset]:
+def setup_datasets(names_to_setup: list[str], wikiann_path: str="wikiann", plank_path: str="plank", split="test") -> list[TestDataset]:
     datasets = []
     for name in names_to_setup:
         try:
@@ -90,5 +91,22 @@ def setup_datasets(names_to_setup: list[str], wikiann_path: str="wikiann", plank
             kwargs["data_path"] = wikiann_path
         elif isinstance(d, Plank):
             kwargs["data_path"] = plank_path
-        d.setup(**kwargs)
+        d.setup(**kwargs, split=split)
     return datasets
+
+if __name__ == '__main__':
+    """ Shows some Data stats """
+    localdata = "../../local_data"
+
+    localdata = os.path.join(sys.path[0], localdata)
+    wikiann_p, plank_p = os.path.join(localdata, "wikiann"), os.path.join(localdata, "plank")
+    log.configure(os.path.join(localdata, "data.log"), "data")
+
+    for split in ("train", "dev", "test"):
+        ds = setup_datasets(("DaNE", "Plank", "WikiANN"), wikiann_path=wikiann_p, plank_path=plank_p, split=split)
+        for d in ds:
+            log(f"{d.name} {split} sentences:", len(d.get_data()[0]))
+    # now for better test statistics
+    for d in ds:
+        for ann in ("ORG", "PER", "LOC", "MISC"):
+            log(f"#{ann} in {d.name}", sum(len([w for w in s if ann in w]) for s in d.get_data()[1]))
