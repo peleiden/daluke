0016e2465be8289fc83fef40631fe353f529d857
Søren Winkel Holm <swholm@protonmail.com>
2021-03-24 13:06:07 +0100

    Split data in general and pretrain

---
+++ daluke/test/pretrain/test_pretrain_data.py
@@ -0,0 +1,25 @@
+import os
+
+from pelutils import MainTest
+
+from daluke.pretrain.data import load_entity_vocab
+
+class TestData(MainTest):
+    def test_entity_loader(self):
+        path = os.path.join(self.test_dir, "entity.jsonl")
+        with open(path, "w") as ev:
+            ev.write("\n".join([
+                '{"id": 1271, "entities": [["27. oktober", "da"]], "count": 529}',
+                '{"id": 1272, "entities": [["Computerprogram", "da"]], "count": 528}',
+                '{"id": 1273, "entities": [["Kancelli", "da"]], "count": 527}',
+                '{"id": 1274, "entities": [["2. marts", "da"]], "count": 527}',
+                '{"id": 1275, "entities": [["Guvern\u00f8r", "da"]], "count": 527}',
+            ]))
+        ev = load_entity_vocab(path)
+        assert ev == {
+            "27. oktober":     { "id": 1271, "count": 529 },
+            "Computerprogram": { "id": 1272, "count": 528 },
+            "Kancelli":        { "id": 1273, "count": 527 },
+            "2. marts":        { "id": 1274, "count": 527 },
+            "Guvern\u00f8r":   { "id": 1275, "count": 527 },
+        }
--- daluke/test/test_data.py
+++ daluke/test/test_data.py
@@ -1,11 +1,7 @@
-import os
-
 import torch
 from transformers import AutoTokenizer
 
-from pelutils import MainTest
-
-from daluke.data import Words, Entities, Example, features_from_str, load_entity_vocab
+from daluke.data import Words, Entities, Example, features_from_str
 from daluke import daBERT
 
 def test_words():
@@ -46,27 +42,3 @@ def test_create_features():
     assert 3 in res.entities.pos[0]
     assert 4 in res.entities.pos[0]
     assert 8 in res.entities.pos[1]
-
-
-class TestData(MainTest):
-
-    def test_entity_loader(self):
-        path = os.path.join(self.test_dir, "entity.jsonl")
-        with open(path, "w") as ev:
-            ev.write("\n".join([
-                '{"id": 1271, "entities": [["27. oktober", "da"]], "count": 529}',
-                '{"id": 1272, "entities": [["Computerprogram", "da"]], "count": 528}',
-                '{"id": 1273, "entities": [["Kancelli", "da"]], "count": 527}',
-                '{"id": 1274, "entities": [["2. marts", "da"]], "count": 527}',
-                '{"id": 1275, "entities": [["Guvern\u00f8r", "da"]], "count": 527}',
-            ]))
-        ev = load_entity_vocab(path)
-        assert ev == {
-            "27. oktober":     { "id": 1271, "count": 529 },
-            "Computerprogram": { "id": 1272, "count": 528 },
-            "Kancelli":        { "id": 1273, "count": 527 },
-            "2. marts":        { "id": 1274, "count": 527 },
-            "Guvern\u00f8r":   { "id": 1275, "count": 527 },
-        }
-
-
