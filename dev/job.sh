b8f8f25ad59623ab5f345cf8d287ec7ee9e4f9cf
Soren Winkel Holm <swholm@protonmail.com>
2021-04-13 17:33:31 +0200

    Update job scripts

--- dev/job.sh
+++ dev/job.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
-#BSUB -q gpuv100
-#BSUB -R "select[gpu32gb]"
-#BSUB -gpu "num=1"
-#BSUB -J "LUKE-finetune"
-#BSUB -R "rusage[mem=20GB]"
-#BSUB -n 1
-#BSUB -W 12:00
-#BSUB -o $HOME/joblogs/%J.out
-#BSUB -e $HOME/joblogs/%J.err
+#BSUB -q gpua100
+#BSUB -R "select[gpu40gb]"
+#BSUB -gpu "num=2"
+#BSUB -J "LUKE-pre"
+#BSUB -R "rusage[mem=60GB]"
+#BSUB -n 2
+#BSUB -W 24:00
 #BSUB -u s183911@student.dtu.dk
 #BSUB -N
 
-echo "Running job"
+DATA_PATH=/work3/$USER/pdata
 
-python3 daluke/run_ner.py local_data/daluketest -m local_data/daluke.tar.gz
+echo "PRETRAIN"
+daluke/pretrain/run.py\
+    $DATA_PATH\
+    -c local_data/training.ini
 
-echo "Completed job"
+echo "Finished job"
--- dev/pretrain.sh
+++ dev/pretrain.sh
@@ -1,20 +1,6 @@
-DATA_PATH=data
+DATA_PATH=/work3/$USER/pdata
 
 echo "PRETRAIN"
-python3 -m luke.cli pretrain\
-    $DATA_PATH/da-pretrain-dataset\
-    /work3/$USER/$DATA_PATH\
-    --bert-model-name "Maltehb/danish-bert-botxo"\
-    --num-epochs 20\
-    --log-dir $DATA_PATH/logs\
-    --learning-rate "5e-5"\
-    --batch-size 2048\
-    --parallel\
-    --fp16
-
-
-#BERT models "roberta-base"/"Maltehb/danish-bert-botxo"
-
-## Questions ##
-#1) Which of the steps are needed? interwiki failed and I did not run multilingual-entity-vocab
-#2) How do I get the danish tokenizer (e.g. https://huggingface.co/Maltehb/danish-bert-botxo)? This failed as I had too old transformers version, but with the newest transformers version, the luke code failed.
+daluke/pretrain/run.py\
+    $DATA_PATH\
+    -c local_data/training.ini
