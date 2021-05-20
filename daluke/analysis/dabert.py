176618b05fc04962945fc841fa8f872b09122b54
asgerius <asger.s@protonmail.com>
2021-05-20 12:44:23 +0200

    Get daBERT MLM accuracy

--- daluke/analysis/dabert.py
+++ daluke/analysis/dabert.py
@@ -1,10 +1,10 @@
 import json
 import os
 
-import torch
-from torch.utils.data import RandomSampler
 
 import click
+import numpy as np
+import torch
 from pelutils import log
 from pelutils.ds import no_grad
 from tqdm import tqdm
@@ -18,7 +18,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 @click.command()
 @click.argument("datadir")
-@click.argument("--ff-size", default=32)
+@click.option("--ff-size", default=32)
 @no_grad
 def word_preds(datadir: str, ff_size: int):
     log.configure(os.path.join(datadir, "dabert-word-preds.log"), "daBERT word predictions")
@@ -26,7 +26,7 @@ def word_preds(datadir: str, ff_size: int):
     with open(os.path.join(datadir, DatasetBuilder.metadata_file)) as f:
         metadata = json.load(f)
     log("Loading model")
-    dabert = AutoModelForPreTraining.from_pretrained("Maltehb/danish-bert-botxo")
+    dabert = AutoModelForPreTraining.from_pretrained("Maltehb/danish-bert-botxo").to(device)
     log("Loading data")
     dataloader = DataLoader(
         datadir,
@@ -34,8 +34,21 @@ def word_preds(datadir: str, ff_size: int):
         dict(),
         device,
     )
-    loader = dataloader.get_dataloader(ff_size, RandomSampler([ex.words for ex in dataloader.examples]))
+    loader = dataloader.get_dataloader(ff_size, None)
     log("Forward passing")
+    correct_preds = np.zeros(len(loader))
     for i, batch in tqdm(enumerate(loader), total=len(loader)):
-        breakpoint()
+        logits = dabert(batch.words.ids).prediction_logits
+        masked_logits = logits[batch.word_mask]
+        preds = masked_logits.argmax(dim=1)
+        correct_preds[i] = (preds == batch.word_mask_labels).float().mean().cpu()
+    log(
+        "MLM token prediction accuracy",
+        "  Mean: %.4f %%" % (100 * correct_preds.mean()),
+        "  Std.: %.4f %%" % (100 * correct_preds.std(ddof=1)),
+    )
 
+
+if __name__ == "__main__":
+    with log.log_errors:
+        word_preds()
