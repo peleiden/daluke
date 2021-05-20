ffa872d9e21da43f5dc2d82717d74f9d2f0c2d97
Søren Winkel Holm <swholm@protonmail.com>
2021-05-20 13:47:38 +0200

    Save NER jobs in subfolders
    Also clean up experiment running code to standardize both train and eval
    
    Resolves #51

---
+++ daluke/ner/__init__.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+from typing import Any, Type
+
+import torch
+from transformers import AutoConfig
+
+import daluke.ner.data as datasets
+from daluke.ner.data import NERDataset
+from daluke.ner.model import NERDaLUKE, get_ent_embed
+
+def load_dataset(entity_vocab: list[dict], args: dict[str, Any], metadata: dict[str, Any], device: torch.device) -> NERDataset:
+    dataset_cls: Type[NERDataset] = getattr(datasets, args["dataset"])
+    return dataset_cls(
+        entity_vocab,
+        base_model      = metadata["base-model"],
+        max_seq_length  = metadata["max-seq-length"],
+        max_entities    = metadata["max-entities"] if args["max_entities"] is None else args["max_entities"],
+        max_entity_span = metadata["max-entity-span"] if args["max_entity_span"] is None else args["max_entity_span"],
+        device          = device,
+    )
+
+def load_model(state_dict: dict, dataset: NERDataset, metadata: dict[str, Any], device: torch.device, entity_embedding_size: int=None) -> NERDaLUKE:
+    bert_config = AutoConfig.from_pretrained(metadata["base-model"])
+    model = NERDaLUKE(
+        len(dataset.all_labels),
+        bert_config,
+        ent_vocab_size = 2, # Same reason as mutate_for_ner
+        ent_embed_size = entity_embedding_size if entity_embedding_size is not None else get_ent_embed(state_dict).shape[1],
+    )
+    model.load_state_dict(state_dict, strict=False)
+    return model.to(device)
--- daluke/ner/run_eval.py
+++ daluke/ner/run_eval.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
 from __future__ import annotations
-from typing import Type
 import os
+from typing import Any
 
 import torch
-from transformers import AutoConfig
 from pelutils import log, Levels, Parser
 
-import daluke.ner.data as datasets
-
-from daluke.ner.model import NERDaLUKE, get_ent_embed
-from daluke.ner.data import NERDataset, Split
+from daluke.ner import load_dataset, load_model
+from daluke.ner.data import Split
 from daluke.ner.evaluation import evaluate_ner, type_distribution
 
 from daluke.serialize import load_from_archive, TRAIN_OUT
@@ -22,50 +19,40 @@ ARGUMENTS = {
         "help": "directory or .tar.gz file containing fine-tuned model, metadata and entity vocab",
         "default": os.path.join("local_data", TRAIN_OUT)
     },
-    "max-entity-span":  {"help": "Max. length of spans used in data. If not given, use the one in pre-training metadata",
-                            "default": None, "type": int},
-    "max-entities":     {"help": "Max. enitites in each example. If not given, use the one in pre-training metadata",
+    "max-entity-span": {"help": "Max. length of spans used in data. If not given, use the one in pre-training metadata",
+                           "default": None, "type": int},
+    "max-entities":    {"help": "Max. enitites in each example. If not given, use the one in pre-training metadata",
                             "default": None, "type": int},
     "quieter":    {"help": "Don't show debug logging", "action": "store_true"},
     "cpu":        {"help": "Run experiment on cpu",    "action": "store_true"},
     "dataset":    {"help": "Which dataset to use. Currently, only DaNE supported", "default": "DaNE"},
 }
 
-def run_experiment(args: dict[str, str]):
+def run_experiment(args: dict[str, Any]):
     device = torch.device("cpu") if args["cpu"] or not torch.cuda.is_available() else torch.device("cuda")
     entity_vocab, metadata, state_dict = load_from_archive(args["model"])
 
     log("Loading dataset ...")
-    dataset_cls: Type[NERDataset] = getattr(datasets, args["dataset"])
-    dataset = dataset_cls(
-        entity_vocab,
-        base_model      = metadata["base-model"],
-        max_seq_length  = metadata["max-seq-length"],
-        max_entities    = metadata["max-entities"] if args["max_entities"] is None else args["max_entities"],
-        max_entity_span = metadata["max-entity-span"] if args["max_entity_span"] is None else args["max_entity_span"],
-        device          = device,
-    )
+    dataset = load_dataset(entity_vocab, args, metadata, device)
     dataloader = dataset.build(Split.TEST, EVAL_BATCH_SIZE)
 
     log("Loading model ...")
-    bert_config = AutoConfig.from_pretrained(metadata["base-model"])
-    ent_embed_size = get_ent_embed(state_dict).shape[1]
-    model = NERDaLUKE(len(dataset.all_labels), bert_config, ent_vocab_size=2, ent_embed_size=ent_embed_size)
-    model.load_state_dict(state_dict, strict=False)
-    model = model.to(device)
+    model = load_model(state_dict, dataset, metadata, device)
 
+    # Print some important information to stdout
     log.debug(model)
     dataset.document(dataloader, Split.TEST)
     type_distribution(dataset.annotations[Split.TEST])
 
     log("Starting evaluation of daLUKE for NER")
     results = evaluate_ner(model, dataloader, dataset, device, Split.TEST)
+
     results.save(args["location"])
     type_distribution(results.preds)
 
 if __name__ == '__main__':
     with log.log_errors:
-        parser = Parser(ARGUMENTS, name="daLUKE-NER-eval", multiple_jobs=False)
+        parser = Parser(ARGUMENTS, name="daluke-ner-eval", multiple_jobs=False)
         experiments = parser.parse()
         parser.document_settings()
         log.configure(
