db4951dbd1a0255b78fb74663d3f96af1f90ccca
Søren Winkel Holm <swholm@protonmail.com>
2021-06-01 17:02:00 +0200

    Print representation examples

---
+++ daluke/ner/analysis/representation_examples.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+import os
+
+import torch
+import numpy as np
+
+import click
+
+from pelutils import log, Levels, set_seeds
+
+from daluke.ner import load_dataset
+from daluke.ner.data import Sequences, Split
+
+from daluke.ner.analysis.representation_geometry import GeometryResults
+
+# {field: axis}
+DIVISIONS = {
+    "pca_transformed" : 0,
+    "umap_transformed": 0,
+    "tsne_transformed": 1,
+}
+
+DUMMY_METADATA = {
+    "max-seq-length": None,
+    "max-entities": None,
+    "max-entity-span": None,
+    "base-model": "Maltehb/danish-bert-botxo",
+}
+
+
+def _show_examples(res: GeometryResults, X: np.ndarray, I: np.ndarray, data: Sequences):
+    for i, idx in enumerate(I):
+        num, span = res.content[idx]["text_num"], res.content[idx]["span"]
+        t, a = [*data.texts[num]], data.annotations[num]
+        t.insert(span[0], "{")
+        t.insert(span[1]+1, "}")
+        t = " ".join(t)
+        log(f"{i} ({X[i]}) {a[span[0]].split('-')[1]}: {t}\n", with_info=False)
+
+@click.command()
+@click.argument("path")
+@click.option("--n", type=int, default=100)
+def main(path: str, n: int):
+    log.configure(
+        os.path.join(path, "geometry-examples.log"), "daLUKE examples",
+        print_level=Levels.DEBUG
+    )
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # Hardcoded to train
+    data = load_dataset(dict(dataset="DaNE"), DUMMY_METADATA, device).data[Split.TRAIN]
+    set_seeds()
+    res = GeometryResults.load(path)
+    for field, axis in DIVISIONS.items():
+        log.section(field)
+        X = getattr(res, field)
+        order = X[:, axis].argsort()
+
+        log(f"Examples where dim. {axis} is high")
+        _show_examples(res, X, order[::-1][:n], data)
+        log(f"Examples where dim. {axis} is low")
+        _show_examples(res, X, order[:n], data)
+
+if __name__ == "__main__":
+    with log.log_errors:
+        main()
