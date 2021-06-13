e90a5f9333bd5e3281e4c8da9c06e611f065f7a0
asgerius <asger.s@protonmail.com>
2021-06-13 16:09:21 +0200

    Confusion matrix with NER predictions
    Resolves #90

--- daluke/ner/evaluation.py
+++ daluke/ner/evaluation.py
@@ -11,7 +11,7 @@ from tqdm import tqdm
 import torch.nn.functional as F
 from seqeval.metrics import classification_report
 
-from pelutils import DataStorage, log
+from pelutils import DataStorage, Table, log
 from pelutils.ds import no_grad
 
 from daluke.ner.model import span_probs_to_preds
@@ -25,10 +25,37 @@ class NER_Results(DataStorage):
     preds:      list[list[str]]
     span_probs: list[dict[tuple[int], np.ndarray]]
 
-    statistics:         dict[str, dict[str, float]]
-    statistics_nomisc:  dict[str, dict[str, float]]
+    statistics:        dict[str, dict[str, float]]
+    statistics_nomisc: dict[str, dict[str, float]]
+
+    # Prediction distribution: confusion_matrix["MISC"]["PER"] is the amount of times PER was predicted when MISC was the correct label
+    confusion_matrix: dict[str, dict[str, int]]
+    confusion_matrix_nomisc: dict[str, dict[str, int]]
 
     subfolder = "eval-results"
+
+def _remove_iob(label: str) -> str:
+    return label[2:] if "-" in label else label
+
+def _format_confmat(confmat: dict[str, dict[str, int]]) -> Table:
+    t = Table()
+    t.add_header([""] + list(confmat))
+    for true_class, preds in confmat.items():
+        t.add_row([
+            true_class,
+            *preds.values(),
+        ])
+    return t
+
+def confusion_matrix(annotations, preds) -> dict[str, dict[str, int]]:
+    """ Builds a confusion matrix with true and predicted labels """
+    annotations = [x for y in annotations for x in y]
+    preds = [x for y in preds for x in y]
+    classes = { _remove_iob(x) for x in preds }
+    confmat = { c: { c_: 0 for c_ in classes } for c in classes }
+    for ann, pred in zip(annotations, preds):
+        confmat[_remove_iob(ann)][_remove_iob(pred)] += 1
+    return confmat
 
 @no_grad
 def evaluate_ner(model: nn.Module, dataloader: torch.utils.data.DataLoader, dataset: NERDataset, device: torch.device, split: Split, also_no_misc=True) -> NER_Results:
@@ -50,6 +77,9 @@ def evaluate_ner(model: nn.Module, dataloader: torch.utils.data.DataLoader, data
         classification_report(annotations, preds, output_dict=True, zero_division=0)
     )
     log(classification_report(annotations, preds, zero_division=0, digits=4))
+    confmat = confusion_matrix(annotations, preds)
+    confmat_nomisc = dict()
+    log("Prediction distribution", _format_confmat(confmat))
 
     if also_no_misc:
         #FIXME: Do this manually instead of rerunning everything
@@ -57,12 +87,16 @@ def evaluate_ner(model: nn.Module, dataloader: torch.utils.data.DataLoader, data
             classification_report(_rm_misc(annotations, dataset.null_label), _rm_misc(preds, dataset.null_label), output_dict=True)
         )
         log(classification_report(_rm_misc(annotations, dataset.null_label), _rm_misc(preds, dataset.null_label), digits=4))
+        confmat_nomisc = confusion_matrix(_rm_misc(annotations, dataset.null_label), _rm_misc(preds, dataset.null_label))
+        log("Prediction distribution", _format_confmat(confmat))
 
     return NER_Results(
         preds=preds,
         span_probs=span_probs,
         statistics=stats,
         statistics_nomisc=stats_nomisc if also_no_misc else {},
+        confusion_matrix=confmat,
+        confusion_matrix_nomisc=confmat_nomisc,
     )
 
 def type_distribution(seqs: list[list[str]]):
