22291753a13470970745d12336d021aee5982981
Søren Winkel Holm <swholm@protonmail.com>
2021-09-15 21:12:20 +0200

    Add history file

---
+++ HISTORY.md
@@ -0,0 +1,13 @@
+# History
+
+## 0.0.4
+    - Added a Python API for maintaining a stateful model and performing CWR, MLM and NER predictions
+
+## 0.0.3: Finalization of Bachelor's Project
+    - Allowed specifying entity spans in masked word prediction CLI
+
+## 0.0.2
+    - CLI made working on Windows
+
+## 0.0.1
+    - Simple single-example CLI released
--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
     readme = f.read()
 
+with open("HISTORY.md") as history_file:
+    history = history_file.read()
+
 with open('requirements.txt') as f:
     required = [
         l.strip() for l in f.read().splitlines() if l.strip() and not l.strip().startswith("#")
@@ -14,9 +17,10 @@ with open('optional-requirements.txt') as f:
 
 # To update
 # 1. Increment version below
-# 2. `rm -rf dist`
-# 3. `python setup.py sdist bdist_wheel`
-# 4. `twine upload dist/*`
+# 2. Document new changes in HISTORY.md
+# 3. `rm -rf dist`
+# 4. `python setup.py sdist bdist_wheel`
+# 5. `twine upload dist/*`
 # To install locally in debug
 # From root of repository, run
 # `pip install --editable --upgrade .`
@@ -34,7 +38,7 @@ setup_args = dict(
     entry_points                  = {"console_scripts": ["daluke = daluke.api.cli:main"]},
     keywords                      = [ "nlp", "ai", "pytorch", "ner" ],
     description                   = "A Danish-speaking language model with entity-aware self-attention",
-    long_description              = readme,
+    long_description              = readme + "\n\n" + history,
     long_description_content_type = "text/markdown",
     license                       = "MIT License",
 )
