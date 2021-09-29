491956cb015ac58b0413e2f453d9eebb85add505
Asger Schultz <asger.s@protonmail.com>
2021-09-29 11:33:27 +0200

    Also save sequences without entities

--- dev/build_data.sh
+++ dev/build_data.sh
@@ -1,31 +1,28 @@
 # Run from daluke repo
 DATA_PATH=/work3/$USER/pdata3
 DUMP_FILE=dump.db
-TOKENIZER=xlm-roberta-base
-# ^ One of xlm-roberta-base, xlm-roberta-large, xlm-roberta-large-finetuned-conll02-dutch,
-# xlm-roberta-large-finetuned-conll02-spanish, xlm-roberta-large-finetuned-conll03-english,
-# xlm-roberta-large-finetuned-conll03-german, Maltehb/danish-bert-botxo, etc.
-PREPROCESS=repeat-entities  # default or repeat-entities
+TOKENIZER=xlm-roberta-base  # Must exist on hugging face
 WIKIDATE=20210901
+PREPROCESS=repeat-entities
 
 DALUKE=$PWD
 LUKE=$DALUKE/luke
 export PYTHONPATH=$PYTHONPATH:$DALUKE:$LUKE
 
-echo "DOWNLOADING WIKIDUMP"
 mkdir -p $DATA_PATH
 cd $DATA_PATH
 if [ ! -f dawiki-$WIKIDATE-pages-articles.xml.bz2 ];
 then
+    echo "DOWNLOADING WIKIDUMP"
     wget https://dumps.wikimedia.org/dawiki/$WIKIDATE/dawiki-$WIKIDATE-pages-articles.xml.bz2
 fi
 if [ ! -d dagw ];
 then
+    echo "DOWNLOADING GIGAWORD"
     wget https://bit.ly/danishgigaword10
     unzip dagw_v1.0-release.zip
     rm dagw_v1.0-release.zip
 fi
-cd $DALUKE
 
 echo "BUILD TEMPORARY DUMP DATABASE"
 cd $LUKE
@@ -41,7 +38,7 @@ python3 -m luke.cli build-entity-vocab\
 echo "PREPROCESSING WIKIDUMP"
 cd $DALUKE
 python3 daluke/pretrain/data/preprocess.py\
-    $DATA_PATH/dawiki-20210901-pages-articles.xml.bz2\
+    $DATA_PATH/dawiki-$WIKIDATE-pages-articles.xml.bz2\
     --function $PREPROCESS\
     --entity-vocab-file $DATA_PATH/entity-vocab.jsonl\
     --dagw-sections $DATA_PATH/dagw/sektioner
