148751df703bdf7ac2c9f914101a0dd11060edd6
Søren Winkel Holm <swholm@protonmail.com>
2021-10-24 22:35:49 +0200

    Serialize token map

--- daluke/collect_modelfile.py
+++ daluke/collect_modelfile.py
@@ -3,8 +3,10 @@
 A convenience script to take the output of the luke pretraining and tar-gz it
 for easy loading
 """
+from daluke.pretrain.data.build import DatasetBuilder
 import os
 import re
+import json
 import shutil
 import subprocess
 import tarfile
@@ -12,8 +14,8 @@ from argparse import ArgumentParser
 
 from pelutils import log, Levels
 
-from daluke.serialize import MODEL_OUT, VOCAB_FILE, METADATA_FILE
 from daluke.pretrain.train import MODEL_OUT as MODEL_FILE
+from daluke.serialize import MODEL_OUT, VOCAB_FILE, METADATA_FILE, TOKEN_MAP_FILE
 
 def _natural_sort(L: list) -> list:
     """ Natural sorting from https://stackoverflow.com/a/37036428 """
@@ -49,31 +51,39 @@ def main():
     modelpath = args.inpath if os.path.isdir(args.inpath) else os.path.dirname(args.inpath)
     vocabfile, metafile = os.path.join(modelpath, "..", VOCAB_FILE), os.path.join(modelpath, "..", METADATA_FILE)
     modelfile = os.path.join(args.inpath, _get_newest_model(args.inpath)) if os.path.isdir(args.inpath) else args.inpath
-    log.debug(f"Using {modelfile}, {vocabfile}, {metafile}")
 
     os.makedirs(os.path.split(args.outpath)[0], exist_ok=True)
+
+    ins, outs = [vocabfile, metafile, modelfile], [VOCAB_FILE, METADATA_FILE, MODEL_OUT]
+    # If reduction is used, also collect the token map
+    with open(metafile, "r") as f:
+        is_reduced = json.load(f).get("reduced-vocab")
+    if is_reduced:
+        ins.append(os.path.join(modelpath, "..", DatasetBuilder.token_map_file))
+        outs.append(TOKEN_MAP_FILE)
+    log.debug(f"Using:", *ins)
 
     # Operate directly on disk as opposed to serialize.save_to_archive which requires us to load the data into mem.
     if shutil.which("tar"):
         log.debug(f"Compressing to {args.outpath} using system tar tool...")
         try:
-            for f, n in zip((vocabfile, metafile, modelfile), (VOCAB_FILE, METADATA_FILE, MODEL_OUT)):
+            for f, n in zip(ins, outs):
                 shutil.copy2(f, n)
             p = subprocess.Popen(
-                ["tar", "-czvf", args.outpath, VOCAB_FILE, METADATA_FILE, MODEL_OUT],
+                ["tar", "-czvf", args.outpath] + outs,
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
             )
             p.wait()
         finally:
-            for n in VOCAB_FILE, METADATA_FILE, MODEL_OUT:
+            for n in outs:
                 try:
                     os.remove(n)
                 except FileNotFoundError:
                     pass
     else:
         with tarfile.open(args.outpath, "w:gz") as tar:
-            for f, n in zip((vocabfile, metafile, modelfile), (VOCAB_FILE, METADATA_FILE, MODEL_OUT)):
+            for f, n in zip(ins, outs):
                 log.debug(f"Compressing {f} as {n} using build-in tar module (may take a while)...")
                 tar.add(f, arcname=n)
     log("Succesfully compressed file saved to", args.outpath)
--- daluke/serialize.py
+++ daluke/serialize.py
@@ -4,10 +4,12 @@ import os
 import shutil
 import tarfile
 import tempfile
+from typing import Optional
 from subprocess import Popen, PIPE
 
 import torch
 from torch import nn
+import numpy as np
 
 from pelutils import log
 
@@ -15,15 +17,17 @@ TRAIN_OUT  = "daluke_ner.tar.gz"
 TRAIN_OUT_BEST = "daluke_ner_best.tar.gz"
 MODEL_OUT = "model.bin"
 
-VOCAB_FILE    = "entity-vocab.json"
-METADATA_FILE = "metadata.json"
+VOCAB_FILE     = "entity-vocab.json"
+METADATA_FILE  = "metadata.json"
+TOKEN_MAP_FILE = "token-map.npy"
 
-def load_from_archive(model: str) -> tuple[list[dict], dict[str, int | str], dict]:
+def load_from_archive(model: str) -> tuple[list[dict], dict[str, int | str], dict, Optional[np.ndarray]]:
     """
     Reads the *.tar.gz archive containing the model, the entity vocabulary and metadata
     """
     if not os.path.isfile(model):
         raise FileNotFoundError(f"Model archive file '{model}' not found.")
+    token_map = None
     if shutil.which("tar"):
         # tar exists in system and is used for decompressing
         log.debug(f"Extracting {VOCAB_FILE}, {METADATA_FILE}, and {MODEL_OUT} using system tar tool...")
@@ -42,9 +46,11 @@ def load_from_archive(model: str) -> tuple[list[dict], dict[str, int | str], dic
                 metadata = json.load(metafile)
             with open(os.path.join(tmpdir, MODEL_OUT), "rb") as mfile:
                 state_dict = torch.load(mfile, map_location="cpu")
+            if metadata.get("reduced-vocab"):
+                with open(os.path.join(tmpdir, TOKEN_MAP_FILE), "rb") as tfile:
+                    token_map = np.load(tfile)
         finally:
             shutil.rmtree(tmpdir)
-        return entity_vocab, metadata, state_dict
     else:
         # Use Python's slow build-in tar
         with tarfile.open(model, "r:gz") as tar:
@@ -56,12 +62,20 @@ def load_from_archive(model: str) -> tuple[list[dict], dict[str, int | str], dic
             log.debug(f"Extracting {MODEL_OUT} ...")
             with tar.extractfile(tar.getmember(MODEL_OUT)) as mfile:
                 state_dict = torch.load(mfile, map_location="cpu")
-        return entity_vocab, metadata, state_dict
+            if metadata.get("reduced-vocab"):
+                with tar.extractfile(tar.getmember(TOKEN_MAP_FILE)) as tfile:
+                    token_map = np.load(tfile)
+    return entity_vocab, metadata, state_dict, token_map
 
-def save_to_archive(outfile: str, entity_vocab: list[dict], metadata: dict, model: nn.Module):
+def save_to_archive(outfile: str, entity_vocab: list[dict], metadata: dict, model: nn.Module, token_map: Optional[np.ndarray]):
     outdir = os.path.split(outfile)[0]
+    outs = [VOCAB_FILE, METADATA_FILE, MODEL_OUT]
+    assert metadata.get("reduced-vocab", False) == (token_map is not None), "'reduced-vocab' must be set iff using token map"
+    if metadata.get("reduced-vocab"):
+        outs.append(TOKEN_MAP_FILE)
+
     if shutil.which("tar"):
-        log.debug(f"Compressing {VOCAB_FILE}, {METADATA_FILE}, and {MODEL_OUT} using system tar tool...")
+        log.debug(f"Compressing {','.join(outs)} using system tar tool...")
         tmpdir = os.path.join(outdir, "tmpdir")
         gp = lambda f: os.path.join(tmpdir, f)
         os.makedirs(tmpdir, exist_ok=True)
@@ -72,8 +86,11 @@ def save_to_archive(outfile: str, entity_vocab: list[dict], metadata: dict, mode
                 json.dump(metadata, tmp)
             with open(gp(MODEL_OUT), "wb+") as tmp:
                 torch.save(model.cpu().state_dict(), tmp)
+            if token_map is not None:
+                with open(gp(TOKEN_MAP_FILE), "wb+") as tmp:
+                    np.save(tmp, token_map)
             p = Popen(
-                ["tar", "-czvf", outfile, "-C", tmpdir, VOCAB_FILE, METADATA_FILE, MODEL_OUT],
+                ["tar", "-czvf", outfile, "-C", tmpdir] + outs,
                 stdout=PIPE,
                 stderr=PIPE,
             )
@@ -81,7 +98,7 @@ def save_to_archive(outfile: str, entity_vocab: list[dict], metadata: dict, mode
         finally:
             shutil.rmtree(tmpdir)
     else:
-        log.debug(f"Compressing {VOCAB_FILE}, {METADATA_FILE}, and {MODEL_OUT} using build-in tar module (this will take a while)...")
+        log.debug(f"Compressing {','.join(outs)} using build-in tar module (this will take a while)...")
         with tarfile.open(outfile, "w:gz") as tar:
             with tempfile.NamedTemporaryFile("w+") as tmp:
                 tmp.write(json.dumps(entity_vocab))
@@ -94,3 +111,7 @@ def save_to_archive(outfile: str, entity_vocab: list[dict], metadata: dict, mode
             with tempfile.NamedTemporaryFile("wb+") as tmp:
                 torch.save(model.cpu().state_dict(), tmp)
                 tar.add(tmp.name, arcname=MODEL_OUT)
+            if token_map is not None:
+                with tempfile.NamedTemporaryFile("wb+") as tmp:
+                    np.save(tmp, token_map)
+                    tar.add(tmp.name, arcname=TOKEN_MAP_FILE)
