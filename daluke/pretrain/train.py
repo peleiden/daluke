bf9023ddae763017b240604797a27f1b65488c3a
asgerius <asger.s@protonmail.com>
2021-10-24 23:23:37 +0200

    Fix bug where progress would not be saved on the very last pu

--- daluke/pretrain/train.py
+++ daluke/pretrain/train.py
@@ -357,7 +357,7 @@ def train(
             tt = TT,
         )
 
-    save_pus = set(range(-1, params.parameter_updates, save_every))
+    save_pus = set(range(-1, params.parameter_updates, save_every)).union({params.parameter_updates})
     log("Saving model at parameter updates: %s" % sorted(save_pus),
         "Validating at parameter updates: %s" % res.val_param_updates.tolist())
 
